982ac846bd3fe8e08da390a57dd178c353815cda
Manuelfb17 <manuel988124064@gmail.com>
2025-10-04 10:15:44 -0500

    Update proyecto_web.py

--- proyecto_web.py
+++ proyecto_web.py
@@ -41,27 +41,10 @@ st.markdown(
         background-position: center;
         background-attachment: fixed;
     }
-    /* Banner superior */
-    .banner {
-        width: 100%;
-        overflow: hidden;
-        background-color: white;
-        box-shadow: 0px 2px 6px rgba(0,0,0,0.1);
-        margin-bottom: 25px;
-    }
-    .banner img {
-        width: 100%;
-        height: 300px;
-        object-fit: cover;
-        display: block;
-    }
     .campo-datos {
         margin-bottom: 20px;
     }
     </style>
-    <div class="banner">
-        <img src="https://i.postimg.cc/7PjfgKkz/marco-peruana.png" alt="Marco Peru Banner">
-    </div>
     """,
     unsafe_allow_html=True
 )
